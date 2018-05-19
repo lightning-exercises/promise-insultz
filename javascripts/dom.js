5a2303a30539e3e7d5c7af34b70b069c8fa8e919
Corey Lutrick <coreylutrick@gmail.com>
2018-05-19 11:31:02 -0700

    added panel

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -6,7 +6,6 @@ const writeInsults = (description, noun) =>
   const burn3 = random(0, 111);
   const burn2 = random(0, 40);
   let domString = '';
-  domString += `<div class='insult col-md-3'>`;
   domString +=  `<h1>You are a ${description[burn].text} ${description[burn3].text} ${noun[burn2].text}</h1>`;
   return domString;
 };
