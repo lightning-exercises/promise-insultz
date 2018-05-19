58f815154a0b1739026db644db56c40467103264
Drake Morrison <drakermorrison@gmail.com>
2018-05-19 10:03:51 -0500

    finished coding, onto styling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-// You should probably write some code here
+'use strict';
+const descriptors = require('./descriptors.js');
+const nouns = require('./nouns.js');
+const dataJS = require('./data.js');
+const addEvent = require('./events.js');
+
+const startApp = () => {
+  return Promise.all([descriptors(), nouns(),]).then(data => {
+    dataJS.setDescriptors(data[0]);
+    dataJS.setNouns(data[1]);
+    addEvent();
+  }).catch(console.error.bind(console));
+};
+
+startApp();
